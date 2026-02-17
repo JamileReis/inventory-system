3504d0585d914306e4be76c7b2851b5d2f77e805
Jamile Reis <mile_matos@hotmail.com>
2026-02-17 19:02:55 -0300

    add api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://stockcontrol-production-b89b.up.railway.app"
+  baseURL: "https://stockcontrol-production-b89b.up.railway.app/api"
 });
 
 export default api;
